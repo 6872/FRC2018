29b95346266c07d55fac8ca55e93579cf8ed4411
Why7090 <whyharry9@gmail.com>
2018-02-10 11:11:28 -0500

    Update

--- 6872/src/org/usfirst/frc/team6872/robot/Robot.java
+++ 6872/src/org/usfirst/frc/team6872/robot/Robot.java
@@ -12,9 +12,6 @@ import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import org.usfirst.frc.team6872.robot.commands.*;
 import org.usfirst.frc.team6872.robot.subsystems.*;
 
 /**
